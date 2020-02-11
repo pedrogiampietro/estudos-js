a8211a479ed4e293e18e28e97a748788a1a4d95f
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-02-11 12:40:01 -0300

    11/02/2020

---
+++ fundamentos/objetos.js
@@ -0,0 +1,12 @@
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega'
+prod1.preco = 4998.90
+
+console.log(prod1.preco)
+
+const prod2 = { 
+    nome: 'Camisa Polo',
+    preco: 78.90
+}
+
+console.log(prod2);
